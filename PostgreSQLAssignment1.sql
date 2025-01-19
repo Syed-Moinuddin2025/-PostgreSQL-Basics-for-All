b6e2774f6aad00bdc17d60bbb3c541b03b65ebbe
Syed Moinuddin <s4moin123@gmail.com>
2025-01-19 18:21:34 +0530

    Added PostgreSQLAssignment1.sql

--- PostgreSQLAssignment1.sql
+++ PostgreSQLAssignment1.sql
@@ -30,7 +30,7 @@ WHERE department='IT';
 --Q3: Delete all employees who are older than 34 years.
 DELETE FROM employees
 WHERE age>34;
-SELECT * FROM employees;
+
 --Q4: Add a new column `email` to the `employees` table.
 ALTER TABLE employees
 ADD COLUMN	email VARCHAR(100);
@@ -62,3 +62,36 @@ ORDER by salary DESC;
 
 --Q9: Insert a new employee with the following details: 
 -- ('Raj', 'Singh', 'Marketing', 60000, '2023-09-15', 30)
+INSERT INTO employees (first_name,last_name,dept_name,salary,joining_date,age)
+VALUES ('Raj','Singh','Marketing',60000,'2023-09-15',30);
+
+--Q10: Update age of employee +1 to every employee 
+UPDATE employees
+SET age = age+1;
+
+--Q11: Find all employees who joined after 2022-01-01.
+SELECT * FROM employees
+WHERE joining_date > '2022-01-01';
+
+--Q12: Find the average salary of employees in each department.
+SELECT dept_name, AVG(salary) AS avrage_Salary
+FROM employees
+GROUP BY dept_name;
+--Q13: Find employees who are older than 30 years.
+SELECT * FROM employees 
+WHERE age >30;
+--Q14: Find the highest salary in the IT department.
+SELECT salary FROM employees
+WHERE dept_name = 'IT'
+ORDER By salary  DESC LIMIT 1;
+--Q15: Update the email column for all employees with appropriate values.
+UPDATE employees
+SET email =LOWER (CONCAT(first_name,'@example.com')); 
+--Q16: Find the total number of employees in each department
+SELECT dept_name,COUNT(*) 
+FROM employees
+GROUP BY dept_name;
+--Q17: Sort employees by their joining_date from the newest to the oldest.
+SELECT * FROM employees
+ORDER BY joining_date DESC;
+SELECT * FROM employees ORDER BY employee_id  ASC
