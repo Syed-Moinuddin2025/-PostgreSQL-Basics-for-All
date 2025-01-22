e823d384dde35df9586a361c1dab8d6b098ca87c
Syed Moinuddin <s4moin123@gmail.com>
2025-01-22 21:03:56 +0530

    Types-of-Operators-in-PostgreSQL.sql

--- 13–SQL–Types-of-Operators-in-PostgreSQL.sql
+++ 13–SQL–Types-of-Operators-in-PostgreSQL.sql
@@ -1,11 +1,11 @@
 SELECT * FROM employee2 ORDER BY employee_id ASC;
 
-SELECT first_name, salary,dept_name, 
+--1) Retrieve the first_name, salary, and calculate a 10% bonus on the salary.
+SELECT first_name, salary, 
 	 (salary*0.10) AS Bonus 
-FROM employee2 
-WHERE dept_name = 'HR';
+FROM employee2;
 
-
+--2) Calculate the Annual Salary and Salary Increment by 5% show the monthly new salary as well
 --CALCULATE NEW SALARY
 SELECT first_name, last_name, salary,
 		(salary*12) AS annual_salary,
