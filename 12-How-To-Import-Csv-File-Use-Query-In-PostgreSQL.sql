81228ef0d9808a486d26e5ce4ae57a391dffb7db
Syed Moinuddin <s4moin123@gmail.com>
2025-01-23 22:50:37 +0530

    Import-Csv-File-Use-Query-In-PostgreSQL

---
+++ 12-How-To-Import-Csv-File-Use-Query-In-PostgreSQL.sql
@@ -0,0 +1,23 @@
+-- Drop the table if it already exists
+DROP TABLE IF EXISTS employee2;
+-- Create the employees table
+CREATE TABLE employee2 (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    department VARCHAR(20),
+    salary DECIMAL(10, 2) ,
+    joining_date DATE ,
+    age INT 
+);
+
+COPY
+employee2(employee_id, first_name, last_name,  department, salary, joining_date, age)
+FROM 'D:\GiHubProjects\SQL-Basics-for-All\employee_data_cleaned.csv'
+DELIMITER','
+CSV HEADER;
+
+SELECT * FROM employee2;
+
+
+SELECT * FROM employee2 ORDER BY employee_id  ASC
